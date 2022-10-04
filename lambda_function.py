e3a8460a3b8daad00f4562ec2e846625a8b1f736
Ashish Lagwankar <lgwnk@amazon.com>
2022-10-04 09:16:12 -0400

    Fix minor syntax errors

--- lambda_function.py
+++ lambda_function.py
@@ -18,47 +18,51 @@ import csv
 
 s3 = boto3.client("s3")
 S3_BUCKET = 'deeplens-basic-posturedetector'
-DATA_BUFFER = 60 # Not used but kept for future uses.
-dynamodb = boto3.resource('dynamodb',region_name='us-east-1')
+DATA_BUFFER = 60  # Not used but kept for future uses.
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 statustable = dynamodb.Table('statustable')
 
 # The below are the main definitions for our human boday parts
-keys_Joints = ['Nose', 'Right Eye', 'Left Eye', 'Right Ear','Left Ear','Right Shoulder',
-         'Left Shoulder','Right Elbow','Left Elbow','Right Wrist','Left Wrist','Right Hip',
-         'Left Hip','Right Knee','Left Knee','Right Ankle','Left Ankle']
+keys_Joints = ['Nose', 'Right Eye', 'Left Eye', 'Right Ear', 'Left Ear', 'Right Shoulder',
+               'Left Shoulder', 'Right Elbow', 'Left Elbow', 'Right Wrist', 'Left Wrist', 'Right Hip',
+               'Left Hip', 'Right Knee', 'Left Knee', 'Right Ankle', 'Left Ankle']
 
 BodyForm = {
-    'Right Lower Arm': ['Right Shoulder','Right Elbow'],
-    'Left Lower Arm': ['Left Shoulder','Left Elbow'],
+    'Right Lower Arm': ['Right Shoulder', 'Right Elbow'],
+    'Left Lower Arm': ['Left Shoulder', 'Left Elbow'],
     'Right Upper Arm': ['Right Elbow', 'Right Wrist'],
     'Left Upper Arm': ['Left Elbow', 'Left Wrist'],
     'Right Thigh': ['Right Hip', 'Right Knee'],
     'Left Thigh': ['Left Hip', 'Left Knee'],
     'Hips': ['Left Hip', 'Right Hip'],
     'Shoulders': ['Left Shoulder', 'Right Shoulder'],
-    }
+}
 
 
 # These are the classes used to calculate angles, locations and other metrics specific to yoga usecase
 class Joint():
-    def __init__(self, name, x,y,conf):
+    def __init__(self, name, x, y, conf):
         self.name = name
-        self.coords = (float(x),float(y))
+        self.coords = (float(x), float(y))
         self.conf = float(conf)
+
+
 class Bodypart():
     def __init__(self, name, jointA, jointB):
         self.jointA = jointA
         self.jointB = jointB
         self.name = name
+
     def get_metrics(self):
-        self.vec = [b - a for a,b in zip(self.jointA.coords, self.jointB.coords)]
+        self.vec = [b - a for a, b in zip(self.jointA.coords, self.jointB.coords)]
         self.length = float(math.hypot(self.vec[1], self.vec[0]))
         if self.vec[0] != 0:
-            self.orient = float(math.degrees(math.atan(self.vec[1]/self.vec[0])))
+            self.orient = float(math.degrees(math.atan(self.vec[1] / self.vec[0])))
         else:
             self.orient = 90.0
         self.conf = float(self.jointA.conf * self.jointA.conf)
-        
+
+
 # This calculates the locations of the joints
 def calculate_joints(allJoints):
     paramsJoints = []
@@ -66,9 +70,10 @@ def calculate_joints(allJoints):
         paramsJoints_pp = {}
         for joint_name in keys_Joints:
             joint = allJoints_pp[joint_name]
-            paramsJoints_pp.update({joint_name:{"Coords":joint.coords, "Conf":joint.conf}})
+            paramsJoints_pp.update({joint_name: {"Coords": joint.coords, "Conf": joint.conf}})
         paramsJoints.append(paramsJoints_pp)
     return paramsJoints
+
 
 # This calculates the angles of the body parts
 def calculate_bodyparts(allBodyparts):
@@ -77,45 +82,48 @@ def calculate_bodyparts(allBodyparts):
         paramsBodyparts_pp = {}
         for bodypart_name in BodyForm:
             body = allBodyparts_pp[bodypart_name]
-            paramsBodyparts_pp.update({bodypart_name:{"Angle":body.orient, "Conf":body.conf}})
+            paramsBodyparts_pp.update({bodypart_name: {"Angle": body.orient, "Conf": body.conf}})
         paramsBodyparts.append(paramsBodyparts_pp)
     return paramsBodyparts
-        
-# These are the classes used to calculate angles, locations and other metrics specific to yoga usecase.
-#... 'create_json' replaces the original 'update_state_json' function
-def create_json(pred_coords, confidence, bboxes, scores, client, iot_topic)
 
+
+# These are the classes used to calculate angles, locations and other metrics specific to yoga usecase.
+# ... 'create_json' replaces the original 'update_state_json' function
+def create_json(pred_coords, confidence, bboxes, scores, client, iot_topic):
     # numpy is needed for better calculation of metrics
     pred_coords_clean = pred_coords.asnumpy()
     confidence_clean = confidence.asnumpy()
     bounding_boxs_clean = bboxes.asnumpy()
     scores_clean = scores.asnumpy()
-    
+
     # The following identifies the joints and body part dictionaries for the picture
-    allJoints = [{name:Joint(name,coord[0],coord[1],conf[0]) for name,coord,conf in 
-                  zip(keys_Joints,coord_per_person,conf_per_person)} for coord_per_person, conf_per_person in 
-                 zip(pred_coords_clean,confidence_clean)]
-    allBodyParts = [{bodypart_name:Bodypart(bodypart_name, joint[joint_names[0]], joint[joint_names[1]]) 
-                 for bodypart_name, joint_names in BodyForm.items()} for joint in allJoints]
-       
+    allJoints = [{name: Joint(name, coord[0], coord[1], conf[0]) for name, coord, conf in
+                  zip(keys_Joints, coord_per_person, conf_per_person)} for coord_per_person, conf_per_person in
+                 zip(pred_coords_clean, confidence_clean)]
+    allBodyParts = [{bodypart_name: Bodypart(bodypart_name, joint[joint_names[0]], joint[joint_names[1]])
+                     for bodypart_name, joint_names in BodyForm.items()} for joint in allJoints]
+
     # We also transfer the bounding box
-    keys_BoundingBox = ["X0","Y0", "Width", "Height"]
-    resBoundingBox = [{"BoundingBox":{"Coords":{key:float(value) for key,value in 
-                        zip(keys_BoundingBox,Boundingbox_per_person[0])},"Confidence":float(conf_per_person[0][0])}}
-                          for Boundingbox_per_person, conf_per_person in zip(bounding_boxs_clean,scores_clean)]
+    keys_BoundingBox = ["X0", "Y0", "Width", "Height"]
+    resBoundingBox = [{"BoundingBox": {"Coords": {key: float(value) for key, value in
+                                                  zip(keys_BoundingBox, Boundingbox_per_person[0])},
+                                       "Confidence": float(conf_per_person[0][0])}}
+                      for Boundingbox_per_person, conf_per_person in zip(bounding_boxs_clean, scores_clean)]
 
     # Let's calculate the joint parts and body angles
-    paramsJoints = calculate_joints(allJoints)       
-    paramsBodyparts = calculate_bodyparts(allBodyparts)
-    
-    #Time stamp is added to the output
+    paramsJoints = calculate_joints(allJoints)
+    paramsBodyparts = calculate_bodyparts(allBodyParts)
+
+    # Time stamp is added to the output
     time_now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    
+
     # This is the schema for our JSON
-    res = [{"Timestamp":time_now,"PersonID":person,**Bbox, "Joints":Joint, "Bodyparts":Body} for person,(Bbox,Joint, Body)
-       in enumerate(zip(resBoundingBox, paramsJoints, paramsBodyparts))]
-    
+    res = [{"Timestamp": time_now, "PersonID": person, **Bbox, "Joints": Joint, "Bodyparts": Body} for
+           person, (Bbox, Joint, Body)
+           in enumerate(zip(resBoundingBox, paramsJoints, paramsBodyparts))]
+
     return json.dumps(res)
+
 
 # This is the original lambda handler func. - not touched
 def lambda_handler(event, context):
@@ -123,28 +131,34 @@ def lambda_handler(event, context):
     print("hello world")
     return
 
-def getStatus( key):
+
+def getStatus(key):
     response = statustable.get_item(
-    Key={
-        'statuskey': key
+        Key={
+            'statuskey': key
         }
     )
     statusvalue = response['statusvalue']
     print(statusvalue)
     return statusvalue
 
+
 def setStatus(key, value):
     statustable.update_item(
-    Key={
-        'statuskey': key,
-    },
-    UpdateExpression='SET statusvalue = :statusvalue',
-    ExpressionAttributeValues={
-        ':statusvalue': value
-    }
-)
+        Key={
+            'statuskey': key,
+        },
+        UpdateExpression='SET statusvalue = :statusvalue',
+        ExpressionAttributeValues={
+            ':statusvalue': value
+        }
+    )
+
+
 def write_to_s3(localfile, bucket, objectname):
     s3.upload_file(localfile, bucket, objectname)
+
+
 # This is the original function
 def infinite_infer_run():
     """ Run the DeepLens inference loop frame by frame"""
@@ -188,10 +202,10 @@ def infinite_infer_run():
 
         if pose_input is None:
             print("no person detected")
-            setStatus('personpresent',False)
+            setStatus('personpresent', False)
             continue
         print('person detected)')
-        setStatus('personpresent',"True")
+        setStatus('personpresent', "True")
         print(pose_input.shape)
 
         start = time()
@@ -204,11 +218,12 @@ def infinite_infer_run():
 
         # local_display.set_frame_data(out)
 
-         # Creating JSON
+        # Creating JSON
         start = time()
         json = create_json(coords, confidence, bboxes, scores, client, iot_topic)
         print('--------------Created JSON: {}s'.format(time() - start))
 
         print('===========================.Entire loop took{}s'.format(time() - loopstart))
+
 
 infinite_infer_run()
