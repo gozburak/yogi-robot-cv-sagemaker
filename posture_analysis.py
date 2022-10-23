aea1a91adb801d146d59ca2f5decad8b34b13c44
Ashish Lagwankar <lgwnk@amazon.com>
2022-10-23 16:37:46 -0400

    Refactor

--- posture_analysis.py
+++ posture_analysis.py
@@ -2,9 +2,33 @@ import datetime
 import json
 import math
 
-class PostureAnalysis():
 
-     # The below are the main definitions for our human boday parts
+# These are the classes used to calculate angles, locations and other metrics specific to yoga usecase
+class Joint():
+    def __init__(self, name, x, y, conf):
+        self.name = name
+        self.coords = (float(x), float(y))
+        self.conf = float(conf)
+
+
+class Bodypart():
+    def __init__(self, name, jointA, jointB):
+        self.jointA = jointA
+        self.jointB = jointB
+        self.name = name
+
+    def get_metrics(self):
+        self.vec = [b - a for a, b in zip(self.jointA.coords, self.jointB.coords)]
+        self.length = float(math.hypot(self.vec[1], self.vec[0]))
+        if self.vec[0] != 0:
+            self.orient = float(math.degrees(math.atan(self.vec[1] / self.vec[0])))
+        else:
+            self.orient = 90.0
+        self.conf = float(self.jointA.conf * self.jointA.conf)
+
+
+class PostureAnalysis():
+    # The below are the main definitions for our human boday parts
     keys_Joints = ['Nose', 'Right Eye', 'Left Eye', 'Right Ear', 'Left Ear', 'Right Shoulder',
                    'Left Shoulder', 'Right Elbow', 'Left Elbow', 'Right Wrist', 'Left Wrist', 'Right Hip',
                    'Left Hip', 'Right Knee', 'Left Knee', 'Right Ankle', 'Left Ankle']
@@ -29,42 +53,19 @@ class PostureAnalysis():
                            'Hips': {'GT Angle': 0.0},
                            'Shoulders': {'GT Angle': 00.0}}
 
-    # These are the classes used to calculate angles, locations and other metrics specific to yoga usecase
-    class Joint():
-        def __init__(self, name, x, y, conf):
-            self.name = name
-            self.coords = (float(x), float(y))
-            self.conf = float(conf)
-
-
-    class Bodypart():
-        def __init__(self, name, jointA, jointB):
-            self.jointA = jointA
-            self.jointB = jointB
-            self.name = name
-
-        def get_metrics(self):
-            self.vec = [b - a for a, b in zip(self.jointA.coords, self.jointB.coords)]
-            self.length = float(math.hypot(self.vec[1], self.vec[0]))
-            if self.vec[0] != 0:
-                self.orient = float(math.degrees(math.atan(self.vec[1] / self.vec[0])))
-            else:
-                self.orient = 90.0
-            self.conf = float(self.jointA.conf * self.jointA.conf)
-
     # This build the joints object
-    def get_joints_params(allJoints):
+    def get_joints_params(self, allJoints):
         paramsJoints = []
         for allJoints_pp in allJoints:
             paramsJoints_pp = {}
-            for joint_name in PostureAnalysis.keys_Joints:
+            for joint_name in self.keys_Joints:
                 joint = allJoints_pp[joint_name]
                 paramsJoints_pp.update({joint_name: {"Coords": joint.coords, "Conf": joint.conf}})
             paramsJoints.append(paramsJoints_pp)
         return paramsJoints
 
     # This builds the body parts
-    def get_bodyparts_params(allBodyparts):
+    def get_bodyparts_params(self, allBodyparts):
         paramsBodyparts = []
         for allBodyparts_pp in allBodyparts:
             paramsBodyparts_pp = {}
@@ -75,42 +76,43 @@ class PostureAnalysis():
         return paramsBodyparts
 
     # This calculates orientations
-    def build_body_from_joints(allJoints):
+    def build_body_from_joints(self, allJoints):
         allBodyparts = []
         for joint in allJoints:
             iter_body = {}
-            for bodypart_name, joint_names in PostureAnalysis.BodyForm.items():
-                body = PostureAnalysis.Bodypart(bodypart_name, joint[joint_names[0]], joint[joint_names[1]])
+            for bodypart_name, joint_names in self.BodyForm.items():
+                body = Bodypart(bodypart_name, joint[joint_names[0]], joint[joint_names[1]])
                 body.get_metrics()
                 iter_body.update({bodypart_name: body})
             allBodyparts.append(iter_body)
         return allBodyparts
 
     # This calculates deviations from the ground truth
-    def calculate_deviations(paramsBodyparts):
+    def calculate_deviations(self, paramsBodyparts):
         deviations = []
         for paramsBodyparts_pp in paramsBodyparts:
             deviations_pp = {}
             for bodypart_name, data in paramsBodyparts_pp.items():
-                diff = data['Angle'] - PostureAnalysis.ground_truth_angles[bodypart_name]['GT Angle']
+                diff = data['Angle'] - self.ground_truth_angles[bodypart_name]['GT Angle']
                 deviations_pp.update({bodypart_name: {'Diff': diff}})
             deviations.append(deviations_pp)
         return deviations
 
     # These are the classes used to calculate angles, locations and other metrics specific to yoga usecase.
     # ... 'create_json' replaces the original 'update_state_json' function
-    def create_json(pred_coords, confidence, bboxes, scores, client, iot_topic, session):
+    def create_json(self, pred_coords, confidence, bboxes, scores, client, iot_topic, session):
         # numpy is needed for better calculation of metrics
         pred_coords_clean = pred_coords.asnumpy()
         confidence_clean = confidence.asnumpy()
         bounding_boxs_clean = bboxes.asnumpy()
         scores_clean = scores.asnumpy()
-
+        print("before allJoints")
         # The following identifies the joints and body part dictionaries for the picture
         allJoints = [{name: Joint(name, coord[0], coord[1], conf[0]) for name, coord, conf in
-                      zip(PostureAnalysis.keys_Joints, coord_per_person, conf_per_person)} for coord_per_person, conf_per_person in
+                      zip(self.keys_Joints, coord_per_person, conf_per_person)} for coord_per_person, conf_per_person in
                      zip(pred_coords_clean, confidence_clean)]
-        allBodyParts = PostureAnalysis.build_body_from_joints(allJoints)
+        print("after allJoints")
+        allBodyParts = self.build_body_from_joints(allJoints)
 
         # We also transfer the bounding box
         keys_BoundingBox = ["X0", "Y0", "Width", "Height"]
@@ -120,17 +122,18 @@ class PostureAnalysis():
                           for Boundingbox_per_person, conf_per_person in zip(bounding_boxs_clean, scores_clean)]
 
         # Let's calculate the joint parts and body angles
-        paramsJoints = PostureAnalysis.get_joints_params(allJoints)
-        paramsBodyparts = PostureAnalysis.get_bodyparts_params(allBodyParts)
+        paramsJoints = self.get_joints_params(allJoints)
+        paramsBodyparts = self.get_bodyparts_params(allBodyParts)
 
         # Time stamp is added to the output
         time_now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
         # Calculating deviations
-        deviations = PostureAnalysis.calculate_deviations(paramsBodyparts)
+        deviations = self.calculate_deviations(paramsBodyparts)
 
         # This is the schema for our JSON
-        res = [{"SessionID": session, "Timestamp": time_now, "PersonID": person, **Bbox, "Joints": Joint, "Bodyparts": Body,
+        res = [{"SessionID": session, "Timestamp": time_now, "PersonID": person, **Bbox, "Joints": Joint,
+                "Bodyparts": Body,
                 "Deviations": Devi}
                for person, (Bbox, Joint, Body, Devi) in
                enumerate(zip(resBoundingBox, paramsJoints, paramsBodyparts, deviations))]
