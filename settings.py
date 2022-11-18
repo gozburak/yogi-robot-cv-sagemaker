1982865eb0f5ca1db114f9cdd0647c2b6b9e4abc
Ashish Lagwankar <lgwnk@amazon.com>
2022-11-18 13:50:18 -0500

    LatestChanges

--- settings.py
+++ settings.py
@@ -1,8 +1,3 @@
-# Variables definition
-AWS_SERVER_PUBLIC_KEY = "AKIAXTPS3WTHP3SUJUZE"
-AWS_SERVER_SECRET_KEY = "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"
-REGION_NAME = "US-EAST-1"
-
 DETECTOR='ssd_512_mobilenet1.0_coco'
 POSEMODEL='simple_pose_resnet18_v1b'
 POSEMODEL_SHA='ccd24037'
@@ -10,5 +5,6 @@ POSEMODEL_SHA='ccd24037'
 ENDPOINT = 'a2h8id2qn57my5-ats.iot.us-east-1.amazonaws.com'
 KEY = './connectionproperties/privKey.key'
 CERT = './connectionproperties/thingCert.crt'
+ROOTCERT = './connectionproperties/rootCA.pem'
 TOPIC = 'yogabot/stream'
 CLIENT_ID = 'ashishlaptop'
