63bc349360a26261ebdd5c1e29a41730ffe08fe1
Ashish Lagwankar <lgwnk@amazon.com>
2022-11-21 09:29:04 -0500

    Put back old deprecated model settings and comment them out. This will let us switch back and forth if needed(unlikely, but just want to be safe)

--- settings.py
+++ settings.py
@@ -1,7 +1,11 @@
 DETECTOR='ssd_512_mobilenet1.0_coco'
 POSEMODEL='alpha_pose_resnet101_v1b_coco'
-# POSEMODEL_SHA='ccd24037' <--- Use for simple pose detector
 POSEMODEL_SHA= True #Default
+
+#old settings(deprecated)
+#DETECTOR='ssd_512_mobilenet1.0_coco'
+#POSEMODEL='simple_pose_resnet18_v1b'
+#POSEMODEL_SHA='ccd24037'
 
 ENDPOINT = 'a2h8id2qn57my5-ats.iot.us-east-1.amazonaws.com'
 KEY = './connectionproperties/privKey.key'
